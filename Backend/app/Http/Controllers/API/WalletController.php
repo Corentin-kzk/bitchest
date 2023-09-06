630250b7fbfd386b199a8e906a76ab245c63ebe0
corentin-kzk <corentin.kozk@gmail.com>
2023-09-06 12:01:42 +0200

    feat(back): add cryptoWallet

--- Backend/app/Http/Controllers/API/WalletController.php
+++ Backend/app/Http/Controllers/API/WalletController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
 use App\Models\User;
+use App\Models\Wallet;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 
@@ -11,10 +12,7 @@ class WalletController extends Controller
 {
     public function index()
     {
-        // Récupérez l'utilisateur connecté
-        $user = User::with('wallet.transactions')->find(auth()->user()->id);
-        // Récupérez le wallet de l'utilisateur avec ses transactions
-        // $wallet = $user->wallet()->with('transactions')->first();
-        return response()->json($user);
+        $wallet = Wallet::where('user_id', auth()->user()->id)->with('cryptoWallet.')->get();
+        return response()->json(['wallet' => $wallet]);
     }
 }
